1eeeee2de5362e73cf83a671de25bd90173a0f48
Trevor Anderson <t.anderson@mail.utoronto.ca>
2016-11-21 18:43:07 -0500

    Debugged the way cubes were being drawn in 2D, should work now.

--- box_test.m
+++ box_test.m
@@ -1,5 +1,9 @@
+%Simulate the inputs from our detectors
 o = zeros(12,1);
 o(2) = 1;
+X = 0;
+Y = 0;
+Z = 3;
 final = zeros(566,724,3,50,'uint8');
 angle = (find(o==1)-1)*pi/6;
 points = [1 1 1 1 -1 -1 -1 -1; 1 1 -1 -1 1 1 -1 -1; -1 1 -1 1 -1 1 -1 1; ones(1,8)];
@@ -10,13 +14,16 @@ rot = [cos(angle) 0 sin(angle) 0;
        0 0 0 1;];
 s = [0.5 0 0 0;
      0 0.5 0 0;
-     0 0 18 0;
+     0 0 0.5 0;
      0 0 0 1];
-t = [eye(3) [0; 0; 20]; [0 0 0 1]];
+t = [eye(3) [X; Y; Z]; [0 0 0 1]];
 cube = t*s*rot*cube;
-%     figure();
-%     plot3(cube(1,:),cube(3,:),cube(2,:));
+points = t*s*rot*points;
+%Plot the cube in 3D
+figure();
+plot3(cube(1,:),cube(3,:),cube(2,:));
 
+%Simulate camera parameters
 f = 1000;
 p = 0;
 ang = 0;
@@ -27,13 +34,19 @@ R = [cos(ang) 0 sin(ang) 0;
      0 0 0 1;];
 K = [f 0 320;0 f 240; 0 0 1];
 P = [eye(3) [0;0;0;]];
-cube2D = K*P*R*T*cube;
-cube2D = [cube2D(1,:)./cube2D(3,:);cube2D(2,:)./cube2D(3,:)];
+
+%Project our lines from 3D to 2D
+points2D = K*P*R*T*points;
+points2D = [points2D(1,:)./points2D(3,:);points2D(2,:)./points2D(3,:)];
+[xs,ys] = getcube2D(points2D);
+% xs = points2D(1,:);
+% ys = points2D(2,:);
 img = zeros(480,640);
-figure();
+f = figure;
+figure(f);
 imshow(img);
 hold on;
-plot(cube2D(1,:),cube2D(2,:));
+plot(xs,ys);
 % frame = getframe(gcf);
 % final(:,:,:,i) = frame.cdata;
 % close;
